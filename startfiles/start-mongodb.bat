910c36bd71c847f9f785786a3eaef11c63c83f4e
Alessandro Del Gobbo <aledelgo@interfree.it>
2015-01-14 20:10:16 +0100

    Added code to clean .lock file of MongoDB
    Many time it happens that Mongo is shut down in a badway by Windows Shutdown or for any other reason. This cause the next start of MongoDB to fail and each time i have to go in that folder to delete that lock file.
    
    This additive code check if it is present and delete it before starting Mongo.

--- startfiles/start-mongodb.bat
+++ startfiles/start-mongodb.bat
@@ -27,6 +27,12 @@ IF NOT EXIST "%cd%\logs\mongodb.log" (
     echo. 2>"%cd%\logs\mongodb.log"
 )
 
+IF EXIST "%cd%\data\mongod.lock" (
+    echo.
+    echo Deleting previous .lock file leved from last unclean MongoDB shutdown...
+    del 2>"%cd%\data\mongod.lock"
+)
+
 echo.
 echo Starting MongoDB
      %cd%\bin\mongodb\bin\mongod.exe --config "%cd%\bin\mongodb\mongodb.conf" --logpath "%cd%\logs\mongodb.log" --dbpath "%cd%\bin\mongodb\data\db"
